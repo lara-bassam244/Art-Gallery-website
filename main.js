1c818497fb5e55eda42d603a6bdafee64f373573
Lara Bassam <Lara.sb244@gmail.com>
2024-03-29 04:28:40 +0200

    new stuff COMIN

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //nav-bar for smol menu
-let menuIcon = document.querySelector(".icons");
-let navBar = document.querySelector("nav")
-menuIcon.addEventListener("click", function(){
-    navBar.classList.toggle("active");
+$(document).ready(function() {
+    console.log("menu icon clicked")
+    $(".menu-icon").click(function() {
+        $("nav").toggleClass("active");
+        $(".menu-icon").toggleClass("active");
+    });
+});
 
-})
